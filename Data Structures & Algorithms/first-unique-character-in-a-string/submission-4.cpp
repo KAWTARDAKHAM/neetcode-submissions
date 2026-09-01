ae1b7da1452646401e6b975b3a75bbd310452a09
KAWTARDAKHAM <114149120+KAWTARDAKHAM@users.noreply.github.com>
2026-09-01 17:18:28 +0100

    Add: first-unique-character-in-a-string - submission-4

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n = s.size();
+        unordered_map <char , int >p;
+        for(int i = 0; i< n ; i++){
+            if(p.contains(s[i])){
+                p[s[i]]=n;
+            }else{  p[s[i]] = i;}
+           
+        }
+
+        int res =n;
+        for(auto& it :p){
+            res = min(res, it.second);
+        }
+        return res == n ? -1 :res;     
+    }
+};
